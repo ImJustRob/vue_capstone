0710d6a2943099ff15c1b0f547f77af634ab4d9e
ImJustRob <robertalothian@hotmail.com>
2024-10-29 19:23:05 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import Header from "./Header.vue";
+import Content from "./Content.vue";
+import Footer from "./Footer.vue";
+
+export default {
+  name: "app",
+  components: {
+    Header,
+    Content,
+    Footer,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <Header />
+    <Content />
+    <Footer />
+  </div>
+</template>
+
+<style></style>
