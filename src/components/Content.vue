57f2facd58c785d24de24be5d47ab413565b22ce
ImJustRob <robertalothian@hotmail.com>
2024-10-29 19:58:06 -0400

    add create action

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,10 +1,12 @@
 <script>
   import axios from "axios";
   import ProductsIndex from "./ProductsIndex.vue";
+  import ProductsNew from "./ProductsNew.vue";
 
   export default {
     components: {
       ProductsIndex,
+      ProductsNew,
     },
     data: function () {
       return {
@@ -21,12 +23,24 @@
          this.products = response.data;
        });
      },
+          handleCreateProduct: function (params) {
+       axios
+         .post("http://localhost:3000/products.json", params)
+         .then((response) => {
+           console.log("products create", response);
+           this.products.push(response.data);
+         })
+         .catch((error) => {
+           console.log("products create error", error.response);
+         });
+     },
    },
   };
   </script>
 
   <template>
     <main>
+      <ProductsNew v-on:createProduct="handleCreateProduct" />
       <ProductsIndex v-bind:products="products" />
     </main>
   </template>
